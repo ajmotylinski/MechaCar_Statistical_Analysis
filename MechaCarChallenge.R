f29d0f4c5fa7b9c60a5a8f0be01571b10b07c0f2
ajmotylinski <drew.motylinski@gmail.com>
2022-04-01 18:12:46 -0500

    Deliverable 2 calculations, analysis needed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,13 @@
 library(dplyr)
+
+#MPG
 cars <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=cars)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=cars))
+
+#Suspension Coil
+coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- coil %>% summarize(mean = mean(PSI), median = median(PSI), var(PSI), sd(PSI))
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), var(PSI), sd(PSI))
+
+
