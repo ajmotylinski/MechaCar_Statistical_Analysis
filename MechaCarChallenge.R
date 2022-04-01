0babc5c8fb28988c1c16f6122e795d6357288974
ajmotylinski <drew.motylinski@gmail.com>
2022-04-01 18:39:25 -0500

    Deliverable 3 calculations, analysis needed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,4 +10,11 @@ coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors =
 total_summary <- coil %>% summarize(mean = mean(PSI), median = median(PSI), var(PSI), sd(PSI))
 lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), var(PSI), sd(PSI))
 
+#T-tests
+t.test(x=coil$PSI,mu=1500) #compare sample 
+t.test(x=subset(coil,Manufacturing_Lot=="Lot1")$PSI, mu=1500) #compare sample 
+t.test(x=subset(coil,Manufacturing_Lot=="Lot2")$PSI, mu=1500) #compare sample 
+t.test(x=subset(coil,Manufacturing_Lot=="Lot3")$PSI, mu=1500) #compare sample 
+
+ 
 
